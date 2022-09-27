4a644334cd2b057f63365e2b98dbf4c0110ec005
Alexander Sidorenko <asiddev@protonmail.com>
2022-09-27 12:53:54 -0700

    Implemented the fetcher.js HTTP request with a function to write the file gotten form that URL body. Also edge cases for if the file exists or not with an option to say yes or no with 'y' and any other key

---
+++ fetcher.js
@@ -0,0 +1,53 @@
+const request = require("request");
+const fs = require("fs");
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+// request("https://www.google.com/fdsafsafsa.html", (error, response, body) => {
+//   console.log("error:", error); // Print the error if one occurred
+//   console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+//   console.log("body:", body); // Print the HTML for the Google homepage.
+// });
+
+const args = process.argv.slice(2);
+
+const fetcher = function (args) {
+  let url = args[0];
+  let path = args[1];
+  request(`${url}`, (error, response, body) => {
+    if (error) {
+      console.log(
+        `There was a problem with your URL: ${url} or path: ${path}. Pleaes check again`
+      );
+      process.exit();
+    }
+
+    if (fs.existsSync(path)) {
+      console.log("it already exists", path);
+      rl.question(
+        `There is already a file at ${path} Would you like to overwrite? Press any key other than 'y' to quit:  `,
+        (answer) => {
+          if (answer === "y") {
+            fs.writeFile(`${path}`, body, () => {
+              console.log(
+                `Downloaded and overwritten. ${body.length} bytes to ${path}`
+              );
+              process.exit();
+            });
+          } else {
+            process.exit();
+          }
+        }
+      );
+    } else
+      fs.writeFile(`${path}`, body, () => {
+        console.log(`Downloaded and saved ${body.length} bytes to ${path}`);
+        process.exit();
+      });
+  });
+};
+fetcher(args);
+
+// fetcher("http://www.example.edu/", "index.html");
